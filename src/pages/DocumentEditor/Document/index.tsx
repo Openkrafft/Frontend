a56d4788e0e058da84ef280ceb585f59caeffe52
Mohamed Ali Ben Othmen <ben.othmen5543@gmail.com>
2021-01-10 15:11:55 -0800

    Add 'addsection' action to add sections from add button in tool bar

--- src/pages/DocumentEditor/Document/index.tsx
+++ src/pages/DocumentEditor/Document/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useValues } from 'kea'
 import globalLogic from '../../../logic'
+import editorLogic from '../logic'
 import Header from '../Sections/Header'
 import ContactInfo from '../Sections/ContactInfo'
 import Skills from '../Sections/Skills'
@@ -12,14 +13,27 @@ import './styles.css'
 
 const Document: React.FC = () => {
 	const { zoom } = useValues(globalLogic)
+	const { sections: currentSections } = useValues(editorLogic)
+	const sections: any = {
+		contactInfo: <ContactInfo />,
+		skills: <Skills />,
+		experience: <Experience />,
+		portfolio: <Portfolio />,
+		list: <List />,
+		text: <Text />
+	}
 	return (
 		<PageContainer id='resume-content' style={{ zoom: `${zoom}%` }}>
 			<Header />
-			<ContactInfo />
-			<Skills />
-			<Experience />
+			{currentSections.map((section: string) => (
+				<div key={section}>{sections[section]}</div>
+			))}
 		</PageContainer>
 	)
 }
+
+const Portfolio = () => <div>Portfolio</div>
+const List = () => <div>list</div>
+const Text = () => <div>text</div>
 
 export default Document
